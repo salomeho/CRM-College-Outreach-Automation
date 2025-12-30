4b793ac066fd89d91331fe50aa3fca7d50c7efe9
Salome Ho <salomeho@gatech.edu>
2025-12-31 00:18:43 +0900

    Add files via upload

---
+++ convert.js
@@ -0,0 +1,14 @@
+const csv = require('csvtojson')
+
+module.exports = 
+
+
+.then((jsonObj)=>{
+    console.log(jsonObj);
+    /**
+     * [
+     * 	{a:"1", b:"2", c:"3"},
+     * 	{a:"4", b:"5". c:"6"}
+     * ]
+     */ 
+})
